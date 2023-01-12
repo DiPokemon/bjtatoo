4677eece9002cab346ea81a18fca14149ecb9a37
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2023-01-13 02:37:47 +0300

    наброски иконок

--- index.php
+++ index.php
@@ -1,5 +1,42 @@
-
+<?php
+/*
+ * Template name: Главная
+ */
+?>
 <?php get_header(); ?>
-            
+    <section class="main_frame">
+
+        <div class="icons_bg">
+            <div class="left_side">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/anchor.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/chair.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/diamond.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/eye.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/flower.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/needles.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/skull.png" alt="">
+            </div>
+            <div class="right_side">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/swallow.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/tattoo.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/tattoo_fist.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/tattoo_guitar.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/tattoo_hand.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/tattoo_heart.png" alt="">
+                <img src="<?php echo get_template_directory_uri()?>/static/img/icons/tattoo_studio.png" alt=""> 
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="half_column">
+                
+            </div>
+            <div class="half_column">
+                
+            </div>
+        </div>
+
+    </section>
+
 
 <?php get_footer(); ?>
