c1fd8c13ce9684ea9f976730a5a58382e3d3de2b
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-01-13 10:50:47 +0100

    begin "Questions"

--- index.php
+++ index.php
@@ -39,4 +39,64 @@
     </section>
 
 
+    <section class="page__question question">
+              <div class="question__container container">
+                <div class="question__body">
+                  <!-- <div class="question-triple_stars_left"><img loading="lazy" class="triple-stars" src="<?php echo get_template_directory_uri()?>/static/img/triple_stars.svg" alt="img"></div>                   -->
+                  <div class="question-content">
+                     <div class="question__title">
+                       <h2 class="_h2 question__title_h2 section_title">Вопрос/Ответ</h2>
+                     </div>
+                     <div class="main_block__wrapper">
+                       <div itemscope itemtype="https://schema.org/FAQPage" class="accordion faq_accordion" id="faq_accordion">                        
+                          <input type="checkbox" name="chacor" id="chacor5" />
+                          <label for="chacor5">Что такое SEO?</label>
+                          <div class="acor-body">
+                              <p>Seo (Search Engine Optimization) —  это совокупность работ, 
+                                направленных на внутреннюю и внешнюю  оптимизацию, за счет чего  
+                                происходит продвижение  сайта в ТОП-10 поисковой выдачи Яндекс или Google 
+                                по целевым запросам пользователей. ТОПом считается первая страница поисковика. 
+                                Поднять сайт в ТОП-10 — главная цель Seo-специалистов.</p>
+                          </div>
+                          <input type="checkbox" name="chacor" id="chacor6" />
+                          <label for="chacor6">Когда будет результат от SEO?</label>
+                          <div class="acor-body">
+                              <p>После проведение работ с сайтом поисковикам нужно время, чтобы его проиндексировать.
+                                 Поисковые запросы со временем начинают ранжироваться все выше и выше,
+                                  пока не попадут в  ТОП-10 позиций. 
+                                  Этот временной интервал всегда разный – в среднем от 2 до 6 месяцев.</p>
+                          </div>
+                          <input type="checkbox" name="chacor" id="chacor7" />
+                          <label for="chacor7">Какие гарантии вы можете  предоставить?</label>
+                          <div class="acor-body">
+                              <p>С каждым клиентом мы заключаем официальный договор. С помощью Seo  
+                                выведем Ваш сайт на показатели  70-90% собранных коммерческих ключевых 
+                                запросов в ТОП-10 по Ростову на Дону, а также всей России. Предоставим 
+                                подробную отчетность на каждом из этапов проделываемых работ. Вы можете 
+                                ознакомится с результаты наших успешных проектов  в разделе кейсы.</p>
+                          </div>
+                          <input type="checkbox" name="chacor" id="chacor8" />
+                          <label for="chacor8">Вы работаете только в Ростове на Дону?</label>
+                          <div class="acor-body">
+                              <p>Мы предоставляем услуги Seo как в Ростове на Дону, так и по 
+                                всей России. Также вы можете заказать раскрутку за рубежом.</p>
+                          </div>
+                          <input type="checkbox" name="chacor" id="chacor9" />
+                          <label for="chacor9">Какие работы вы проводите на проекте?</label>
+                          <div class="acor-body">
+                              <p>Команда наших специалистов вносит изменения в контент и функционал сайта. 
+                                Все предварительно согласовывается с клиентом. Мы оптимизируем существующий контент 
+                                и добавляем новый в соответствии с требованиями поисковых систем, при необходимости 
+                                можем менять функционал и структуру посадочных страниц.</p>
+                          </div>
+
+                         </div>
+                      </div>
+                    </div>
+                  <!-- <div class="question-triple_stars_right"><img loading="lazy" class="triple-stars" src="<?php echo get_template_directory_uri()?>/static/img/triple_stars.svg" alt="img"></div> -->
+                </div>
+              </div>
+            </section>
+
+
 <?php get_footer(); ?>
