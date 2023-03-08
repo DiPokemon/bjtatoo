0c4fdab8436b42f67928244534bd68a2a36b268a
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2023-03-08 22:57:06 +0300

    доделка block-map

--- template-parts/block-map.php
+++ template-parts/block-map.php
@@ -2,7 +2,12 @@
 <section>
     <div class="container map_container">
         <div class="half_column map_wrapper" id="map" data-address="<?= $address_city ?>, <?= $address_street ?>, <?= $address_building ?>" data-latitude="<?= $address_latitude ?>" data-longitude="<?= $address_longitude ?>" data-name="<?= $org_name ?>" data-phonefront="<?= $contacts_main_phone_front ?>" data-phonehref="<?= $contacts_main_phone_href ?>"></div>    
-        <div class="half_column">            
+        <div class="half_column contacts_block">         
+            <div class="address"><?= $address_city ?>, <?= $address_street ?>, <?= $address_building ?></div>  
+            <div class="phones">
+                <span><a href="<?= $contacts_main_phone_href ?>"><?= $contacts_main_phone_front ?></a></span>
+                <span><a href="<?= $contacts_add_phone_href ?>"><?= $contacts_add_phone_front ?></a></span>
+            </div>
             <?php get_template_part( 'template-parts/socials' ); ?>
         </div>
     </div>
