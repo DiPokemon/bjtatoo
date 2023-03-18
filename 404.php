368397e61d9cd1d1fa24931c1522bb16815cc1a6
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2023-03-19 00:36:45 +0300

    404 page

--- 404.php
+++ 404.php
@@ -4,22 +4,23 @@
  */
 ?>
 <?php get_header(); ?>
-
+<?php include 'template-parts/variables.php' ?>
+<?php get_template_part( 'template-parts/block', 'pageheader' ); ?>
 <section>
     <div class="container">
-        <div class="page_content_wrapper">
-            <div class="page_content">
-                <?php the_content(); ?>
-            </div>
-
-            <div class="half_column">      
-              <?php get_template_part( 'template-parts/form', 'vertical' ); ?>
-            </div>
-            <?php get_sidebar(); ?>
+        <div class="text_404">
+            Извините, страница не найдена, но Вы можете найти посмотреть, что у нас есть или оставить заявку!
         </div>
+        <div class="page_content_wrapper">
+            <div class="half_column">
+
+            </div>
+            <div class="half_column">
+                <?php get_template_part( 'template-parts/form', 'vertical' ); ?>
+            </div>
+        </div>       
     </div>
 </section>
 
-<?php get_template_part( 'template-parts/block', 'portfolio' ); ?>
 
 <?php get_footer(); ?>
