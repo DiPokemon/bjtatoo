22d48f76a23dd6aa6ea85d08a8a4cf04f50ab33a
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2023-11-18 16:48:08 +0300

    fix ya adv

--- header.php
+++ header.php
@@ -5,8 +5,7 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />    
-        <script>window.yaContextCb=window.yaContextCb||[]</script>  
-        
+        <!-- <script>window.yaContextCb=window.yaContextCb||[]</script> -->
         <?php wp_head();?>
         
     </head>
